58eed08400b4051608b65b736ac59196d9ddf1b4
Abednego <abedikroos15@gmail.com>
2023-03-24 12:50:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function moveDodgerLeft() {
+    var left = parseInt(dodger.style.left)
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+  
+  function moveDodgerRight() {
+    var left = parseInt(dodger.style.left)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+
+  
+  
+  
+  
+  
+  
